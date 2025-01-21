a6776e5853b2a404926f86d9bc6daf667e681699
ajisha-7777 <ajishaias7@gmail.com>
2025-01-21 02:03:40 -0800

    Added index.html or updated CSS styles
    Added the main structure of the website using HTML. Next, I'll upload CSS for styling.

---
+++ scripy.js
@@ -0,0 +1,18 @@
+// Function to start lesson
+function startLesson() {
+    alert("Let's start learning English! You'll learn the basics of greetings and introductions.");
+}
+
+// Function to go to next lesson
+function nextLesson() {
+    alert("Great job! Now, let's move to the next lesson.");
+}
+// Function to start lesson
+function startLesson() {
+    alert("Let's start learning English! You'll learn the basics of verb tenses.");
+}
+
+// Function to go to next lesson
+function nextLesson() {
+    alert("Great job! Now, let's move to the next lesson.");
+}
